f7edc370fe36741ff70c96f8fc42a5ef40b541cc
Dzaky Fatur Rahman <117415944+dzaky2636@users.noreply.github.com>
2025-03-18 14:09:21 +0700

    buttons, laci and news

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,7 @@ class HomePage extends StatelessWidget {
             SizedBox(height: size / 1.5),
             // after carousel widget
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
               child: 
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
@@ -121,12 +121,162 @@ class HomePage extends StatelessWidget {
                         style: TextStyle(
                           fontSize: 10,
                           color: Colors.grey,
-                        ),
-                      )
-                    )
-                  ],
+                    ),
+                  )
                 )
+              ],
+            )
+          ),
+            Container(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+              decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.white, Color.fromARGB(255, 246, 237, 242)],
               ),
+              ),
+              child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(7, (index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Column(
+                  children: [
+                    GestureDetector(
+                    onTap: () {
+                      // button functionality here
+                    },
+                    child: CircleAvatar(
+                      radius: 25,
+                      backgroundImage: AssetImage('assets/images/image_$index.png'),
+                    ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                    'Button $index',
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,
+                    ),
+                    ),
+                  ],
+                  ),
+                );
+                }),
+              ),
+              ),
+            ),
+            SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: 
+                Text(
+                      'Laci',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    )
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(3, (index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Column(
+                  children: [
+                    GestureDetector(
+                    onTap: () {
+                      // button functionality here
+                    },
+                    child: CircleAvatar(
+                      radius: 25,
+                      backgroundImage: AssetImage('assets/images/image_$index.png'),
+                    ),
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                    'Button $index',
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,
+                    ),
+                    ),
+                  ],
+                  ),
+                );
+                }),
+              ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: 
+                Text(
+                      'Siaran Pers Kementrian',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    )
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(5, (index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Card(
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Container(
+                          width: 200,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ClipRRect(
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(10.0),
+                                  topRight: Radius.circular(10.0),
+                                ),
+                                child: Image.asset(
+                                  'assets/images/sentuh_tanahku_logo_withborder.png',
+                                  height: 100,
+                                  width: 200,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  'News Title $index',
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+            ),
         ],
       ),
     );
